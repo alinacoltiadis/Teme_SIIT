b22521d0c8a268ef299712f72c0bba2b153ae4b3
alinacoltiadis <alina.coltiadis@gmail.com>
2019-11-28 05:16:07 +0200

    mesaj de up

--- src/main/java/tema1_ex9/RemoveDuplicatesFromArray.java
+++ src/main/java/tema1_ex9/RemoveDuplicatesFromArray.java
@@ -1,5 +1,6 @@
 package tema1_ex9;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class RemoveDuplicatesFromArray {
@@ -11,26 +12,28 @@ public class RemoveDuplicatesFromArray {
         int arrayLength = readVar.nextInt();
 
         int[] arrayReadFromKeyboard = new int[arrayLength];
-        int[] newArray = new int[];
-        int i=0;
+        boolean isequal = false;
+        int index2 =1;
 
+        System.out.println("Please input array's elements: ");
         for (int index= 0; index < arrayLength; index++) {
             arrayReadFromKeyboard[index] = readVar.nextInt();
         }
 
-        //2.identifying & removing duplicates
-        for (int index1=0; index1 < arrayLength; index1++) {
-            for (int index2=index1+1; index2 < arrayLength; index2++) {
-                if (arrayReadFromKeyboard[index1] == arrayReadFromKeyboard[index2]) {
-                    newArray[i] = arrayReadFromKeyboard[index1];
-                    i++;
-                }
-
+        //2.identifying & printing only uniques elements
+        System.out.println("The unique elements array is: ");
+        int currentElement = arrayReadFromKeyboard[0];
+        for (int index1 = 0; index1 < arrayLength; index1++) {
+            if (currentElement == arrayReadFromKeyboard[index1] && !isequal) {
+                isequal = true;
+            } else if(currentElement != arrayReadFromKeyboard[index1] ) {
+                System.out.print(" " +currentElement);
+                currentElement = arrayReadFromKeyboard[index1];
+                isequal=false;
+                index2++;
             }
         }
-
-
-    }
-
-
+        System.out.println(" "+currentElement);
+        System.out.println("And there are "+index2+" elements in array");
+   }
 }
